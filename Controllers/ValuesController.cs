48a158472f15912a00ad9a666042cef16269e98c
MorinRod <morin.rodenski@cardioscan.com.au>
2019-09-04 10:14:58 +1000

    service changed

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace helloMicroservice.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "Hello", "Service" };
+            return new string[] { "Hello", "Service-changed" };
         }
 
         // GET api/values/5
